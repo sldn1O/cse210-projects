ad354488620d5bcf1fac769c4e2e69e38d599643
Jared Swasey <jared@swaseyfam.com>
2023-09-12 14:07:44 -0600

    Finished Prep 1.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // ask user for first name
+        Console.WriteLine("What is your first name? ");
+        string first = Console.ReadLine();
+
+        //ask user for last name
+        Console.WriteLine("What is your last name? ");
+        string last = Console.ReadLine();
+        
+        //print 007 name
+        Console.WriteLine($"Your name is {last}, {first} {last}.");
     }
+
 }
