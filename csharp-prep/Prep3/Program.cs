deb973b0d6b09d8b150bc31cb81370a2ba42a667
Jared Swasey <jared@swaseyfam.com>
2023-09-18 11:04:38 -0600

    Finished Prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,38 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        // Part 1 and part 2, asking user to put number.
+        // Console.Write("what is the magic number? ");
+        // string magic_number = Console.ReadLine();
+        // int number = int.Parse(magic_number);
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 101);
+
+        int guess = -1;
+        while (guess != number)
+        {
+            Console.Write("what is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
