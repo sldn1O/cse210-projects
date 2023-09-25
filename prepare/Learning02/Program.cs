357c56f06c9d20bc2f06b0273ec49ad11de6f422
Jared Swasey <jared@swaseyfam.com>
2023-09-25 11:00:09 -0600

    Finished Prepare W03

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Item Stocker";
+        job1._company = "Local Supermarket";
+        job1._startYear = 2015;
+        job1._endYear = 2018;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Mystery Solver";
+        job2._company = "Mystery Inc.";
+        job2._startYear = 2018;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Fred Jones";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
