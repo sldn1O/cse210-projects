8ac37be3cb46a816e8b033ecab10de02bd04f695
Jared Swasey <jared@swaseyfam.com>
2023-12-02 22:09:00 -0700

    Finished Foundation 2

---
+++ final/Foundation2/Order.cs
@@ -0,0 +1,45 @@
+class Order
+{
+    private List<Product> products;
+    private Customer customer;
+
+    public Order(Customer customer)
+    {
+        this.products = new List<Product>();
+        this.customer = customer;
+    }
+
+    public void AddProduct(Product product)
+    {
+        products.Add(product);
+    }
+
+    public decimal CalculateTotalCost()
+    {
+        decimal totalCost = 0;
+        foreach (var product in products)
+        {
+            totalCost += product.CalculateTotalPrice();
+        }
+
+        // Shipping cost
+        totalCost += customer.IsInUSA() ? 5 : 35;
+
+        return totalCost;
+    }
+
+    public string GetPackingLabel()
+    {
+        string packingLabel = "Packing Label:\n";
+        foreach (var product in products)
+        {
+            packingLabel += $"{product.GetPackingLabel()}\n";
+        }
+        return packingLabel;
+    }
+
+    public string GetShippingLabel()
+    {
+        return customer.GetShippingLabel();
+    }
+}
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -1,9 +1,40 @@
 using System;
-
+using System.Collections.Generic;
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        // Create addresses
+        Address address1 = new Address("123 Sesame Street", "Sesame City", "California", "USA");
+        Address address2 = new Address("7463 North St", "Kentville", "Nova Scotia", "Canada");
+
+        // Create customers
+        Customer customer1 = new Customer("John Doe", address1);
+        Customer customer2 = new Customer("Jane Doe", address2);
+
+        // Create products
+        Product product1 = new Product("Candles", 7864, 24.99m, 5);
+        Product product2 = new Product("Samsung Galaxy Phone", 1345, 768.98m, 1);
+        Product product3 = new Product("Apples", 7567, 5.50m, 10);
+
+        // Create orders
+        Order order1 = new Order(customer1);
+        order1.AddProduct(product1);
+        order1.AddProduct(product2);
+
+        Order order2 = new Order(customer2);
+        order2.AddProduct(product2);
+        order2.AddProduct(product3);
+
+        // Display results
+        Console.WriteLine("Order 1 Details:");
+        Console.WriteLine(order1.GetPackingLabel());
+        Console.WriteLine(order1.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order1.CalculateTotalCost()}");
+
+        Console.WriteLine("\nOrder 2 Details:");
+        Console.WriteLine(order2.GetPackingLabel());
+        Console.WriteLine(order2.GetShippingLabel());
+        Console.WriteLine($"Total Cost: ${order2.CalculateTotalCost()}");
     }
 }
